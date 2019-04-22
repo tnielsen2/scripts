7df78c357c269968422477284c8430951ed03414
Trent Nielsen <trentnielsen84@gmail.com>
2019-04-22 14:06:15 -0700

    Update aws-snapshot-cleanup.sh

--- aws/aws-snapshot-cleanup.sh
+++ aws/aws-snapshot-cleanup.sh
@@ -8,18 +8,18 @@ QUERY="Snapshots[?StartTime <= '$DATE'].{id:SnapshotId}"
 for REGION in us-west-2 \
               us-east-2
 do
-#Pull down all snapshot IDs that are tagged with the LifeCycle schedule tag of "Midnight" 
-
+#Pull down all snapshot IDs that are tagged with the LifeCycle schedule tag of "Midnight"
 SNAPSHOTS=$(aws ec2 describe-snapshots --region "$REGION" --filters "$FILTER" --query "$QUERY" --output text | grep -e 'snap-.*\w' -o)
 echo -------------------------------
 echo Looping through region $REGION
 echo -------------------------------
-#Loop through the orphaned snapshots
+#Loop through the snapshot and delete
 for SNAPSHOT in $SNAPSHOTS
 do
-echo Snapshot ID $SNAPSHOT in $REGION is older than 14 days. Deleting.
-aws ec2 describe-snapshots --filters Name='snapshot-id',Values=$SNAPSHOT | grep 'StartTime'
+STARTTIME=$(aws ec2 describe-snapshots --filters Name='snapshot-id',Values=$SNAPSHOT | grep StartTime)
+STARTDATE=$(echo $STARTTIME | cut -d":" -f 2 | cut -d 'T' -f 1 | cut -c 3-)
+echo Snapshot ID $SNAPSHOT in $REGION is older than 14 days. Creation date is $STARTDATE. Deleting snapshot $SNAPSHOT.
+aws ec2 delete-snapshot --snapshot-id $SNAPSHOT
 done
 
 done
-
