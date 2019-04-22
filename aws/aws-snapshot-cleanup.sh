3aa9f84fa4bfc8ed13da5ff7a95a6cb2c274c118
Trent Nielsen <trentnielsen84@gmail.com>
2019-04-22 13:24:32 -0700

    Create aws-snapshot-cleanup.sh

---
+++ aws/aws-snapshot-cleanup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+FILTER="Name=tag:aws:dlm:lifecycle-schedule-name,Values=Midnight"
+DATE=$(date -d '14 days ago' +%Y-%m-%d)
+QUERY="Snapshots[?StartTime <= '$DATE'].{id:SnapshotId}"
+
+#Loop through all regions
+for REGION in us-west-2 \
+              us-east-2
+do
+#Pull down all snapshot IDs that are tagged with the LifeCycle schedule tag of "Midnight" 
+
+SNAPSHOTS=$(aws ec2 describe-snapshots --region "$REGION" --filters "$FILTER" --query "$QUERY" --output text | grep -e 'snap-.*\w' -o)
+echo -------------------------------
+echo Looping through region $REGION
+echo -------------------------------
+#Loop through the orphaned snapshots
+for SNAPSHOT in $SNAPSHOTS
+do
+echo Snapshot ID $SNAPSHOT in $REGION is older than 14 days. Deleting.
+aws ec2 describe-snapshots --filters Name='snapshot-id',Values=$SNAPSHOT | grep 'StartTime'
+done
+
+done
+
